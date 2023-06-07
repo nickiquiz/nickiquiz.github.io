0b1002e17ad16a3b319d26305463e4b8cc7f6a7a
chishl <dorahhhf@gmail.com>
2023-06-07 22:48:42 +0800

    fixed question img

--- src/components/QuestionBlock.jsx
+++ src/components/QuestionBlock.jsx
@@ -27,7 +27,7 @@ const QuestionBlock = ({
             disabled={validPick}
 
         >
-            <img src={question.image} alt={question.alt}/>
+            <img src={question.image} alt={question.alt} className="img"/>
             <h3>{question.text}</h3>
             <p>
                 <a href={question.image}>{question.credit} </a>
--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -2,7 +2,7 @@ const Title = ({title, subtitle, banner}) => {
     return (
         <div>
             <h1>{title}</h1>
-            <img src={banner} alt="yas" width="100%" height="auto"/>
+            <img src={banner} alt="yas" width="100%" height="auto" className= "banner"/>
             <p>{subtitle}</p>
         </div>
     )
